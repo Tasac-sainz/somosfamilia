2f77827b98728d38acaf675b0147c18e85a7b235
tasac-sainz <tssainzdeaja@gmail.com>
2025-09-11 20:49:39 +0200

    aplicando npm run build

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,2 +1,2 @@
-console.log(">> Ready :)");
+console.log("ready!");const n=document.querySelector(".burger-menu"),e=document.querySelector(".list"),c=document.querySelectorAll(".list a");n.addEventListener("click",()=>{e.classList.toggle("hidden")});c.forEach(t=>{t.addEventListener("click",s=>{e.classList.add("hidden")})});
 //# sourceMappingURL=main.js.map
