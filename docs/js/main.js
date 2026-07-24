10e6a891e1796b37e766d2b9c56944eeab4f53a1
Tania S. Sainz de Aja <tssainzdeaja@gmail.com>
2026-07-24 10:56:41 +0200

    Deployed new version of the page to production

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,2 +1,13 @@
-console.log("ready!");const n=document.querySelector(".burger-menu"),e=document.querySelector(".list"),c=document.querySelectorAll(".list a");n.addEventListener("click",()=>{e.classList.toggle("hidden")});c.forEach(t=>{t.addEventListener("click",s=>{e.classList.add("hidden")})});
+console.log("ready!");
+const burger = document.querySelector(".burger-menu");
+const menuList = document.querySelector(".list");
+const menuLinks = document.querySelectorAll(".list a");
+burger.addEventListener("click", () => {
+  menuList.classList.toggle("hidden");
+});
+menuLinks.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    menuList.classList.add("hidden");
+  });
+});
 //# sourceMappingURL=main.js.map
